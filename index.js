fcc2549d6dba57facaa70163f5e8824fb1f98b11
Zicheng.S <zichengshan@gmail.com>
2022-03-26 19:31:27 -0700

    Add listeners

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let money = 0
+let task = []
+let taskMoney = []
+const washBtnEl = document.getElementById("buttons-wash")
+const movBtnEl = document.getElementById("buttons-mov")
+const pullBtnEl = document.getElementById("buttons-pull")
+const resetBtnEl = document.getElementById("reset-btn")
+const taskEl = document.getElementById("task")
+
+washBtnEl.addEventListener("click", function () {
+    alert("washBtnEl")
+})
+
+movBtnEl.addEventListener("click", function () {
+    alert("movBtnEl")
+})
+
+pullBtnEl.addEventListener("click", function () {
+    alert("pullBtnEl")
+})
+
+resetBtnEl.addEventListener("click", function () {
+    alert("resetBtnEl")
+})
