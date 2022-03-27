7f9b55001216f2d633eb8fb2b82dd0d72fa300ad
Zicheng.S <zichengshan@gmail.com>
2022-03-26 19:55:34 -0700

    Modify the layout

--- index.js
+++ index.js
@@ -3,22 +3,16 @@ let money = 0
 let task = []
 let taskMoney = []
 const washBtnEl = document.getElementById("buttons-wash")
-const movBtnEl = document.getElementById("buttons-mov")
-const pullBtnEl = document.getElementById("buttons-pull")
 const resetBtnEl = document.getElementById("reset-btn")
+const addBtnEl = document.getElementById("add-btn")
 const taskEl = document.getElementById("task")
 
-washBtnEl.addEventListener("click", function () {
-    alert("washBtnEl")
+
+
+addBtnEl.addEventListener("click", function () {
+    alert("addBtnEl")
 })
 
-movBtnEl.addEventListener("click", function () {
-    alert("movBtnEl")
-})
-
-pullBtnEl.addEventListener("click", function () {
-    alert("pullBtnEl")
-})
 
 resetBtnEl.addEventListener("click", function () {
     alert("resetBtnEl")
