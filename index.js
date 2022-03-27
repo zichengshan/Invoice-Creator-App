8fe7a0c3c54645a6c49ac03bdc275dbd2bf610e9
Zicheng.S <zichengshan@gmail.com>
2022-03-26 20:31:57 -0700

    Finish Add item part && clear the input

--- index.js
+++ index.js
@@ -2,18 +2,39 @@
 let money = 0
 let task = []
 let taskMoney = []
-const washBtnEl = document.getElementById("buttons-wash")
 const resetBtnEl = document.getElementById("reset-btn")
 const addBtnEl = document.getElementById("add-btn")
+const inputsNameEl = document.getElementById("inputs-name")
+const inputsMoneyEl = document.getElementById("inputs-money")
 const taskEl = document.getElementById("task")
 
 
-
+render()
 addBtnEl.addEventListener("click", function () {
-    alert("addBtnEl")
+    task.push(inputsNameEl.value)
+    taskMoney.push(inputsMoneyEl.value)
+    render()
 })
+
+function render() {
+    let innerHtml = ""
+    for(let i = 0; i < taskMoney.length; i++){
+        innerHtml += `
+        <div class="subTitle">
+                <p class="task-name"> ${task[i]} </p>
+                
+                <p class="task-money"> ${taskMoney[i]}</p>
+        </div>
+        `
+    }
+    taskEl.innerHTML = innerHtml
+    inputsNameEl.value = ""
+    inputsMoneyEl.value = ""
+}
 
 
 resetBtnEl.addEventListener("click", function () {
-    alert("resetBtnEl")
+    task = []
+    taskMoney = []
+    render()
 })
