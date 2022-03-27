d52d735c9deb423ddc71fcc83261baedeb1eba75
Zicheng.S <zichengshan@gmail.com>
2022-03-27 10:35:41 -0700

    Reproduce data on refresh

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-
-localStorage.setItem("money", "0")
-localStorage.setItem("task", "[]")
-localStorage.setItem("taskMoney","[]")
+let money = 0, task = [], taskMoney = []
 const resetBtnEl = document.getElementById("reset-btn")
 const addBtnEl = document.getElementById("add-btn")
 const inputsNameEl = document.getElementById("inputs-name")
@@ -9,27 +6,31 @@ const inputsMoneyEl = document.getElementById("inputs-money")
 const taskEl = document.getElementById("task")
 const totalAmountEl = document.getElementById("total-amount")
 
-render()
+let moneyStorage = JSON.parse(localStorage.getItem("money"))
+let taskStorage = JSON.parse(localStorage.getItem("task"))
+let taskMoneyStorage = JSON.parse(localStorage.getItem("taskMoney"))
+
+if(taskStorage){
+    money = moneyStorage
+    task = taskStorage
+    taskMoney = taskMoneyStorage
+    render(money, task, taskMoney)
+}
+
 addBtnEl.addEventListener("click", function () {
     if(parseInt(inputsMoneyEl.value)){
-        let money = parseInt(localStorage.getItem("money")) + parseInt(inputsMoneyEl.value)
-        let task = JSON.parse(localStorage.getItem("task"))
-        let taskMoney = JSON.parse(localStorage.getItem("taskMoney"))
-
-        localStorage.setItem("money", JSON.stringify(money))
+        money += parseInt(inputsMoneyEl.value)
         task.push(inputsNameEl.value)
         taskMoney.push(inputsMoneyEl.value)
-        localStorage.setItem("task", JSON.stringify(task))
-        localStorage.setItem("taskMoney", JSON.stringify(taskMoney))
-        render()
+
+        saveInLocalStorage()
+        render(money, task, taskMoney)
     }else {
         alert("Invalid money input! Please enter a number!")
     }
 })
 
-function render() {
-    let task = JSON.parse(localStorage.getItem("task"))
-    let taskMoney = JSON.parse(localStorage.getItem("taskMoney"))
+function render(money, task, taskMoney) {
     let innerHtml = ""
     for(let i = 0; i < taskMoney.length; i++){
         innerHtml += `
@@ -41,28 +42,29 @@ function render() {
         `
     }
     taskEl.innerHTML = innerHtml
-    totalAmountEl.innerHTML = "$" + localStorage.getItem("money")
+    totalAmountEl.innerHTML = "$" + money
     inputsNameEl.value = ""
     inputsMoneyEl.value = ""
 }
 
 function remove(index){
-    let task = JSON.parse(localStorage.getItem("task"))
-    let taskMoney = JSON.parse(localStorage.getItem("taskMoney"))
-    let money = parseInt(localStorage.getItem("money")) - parseInt(taskMoney[index])
-    localStorage.setItem("money", JSON.stringify(money))
-
+    money -= parseInt(taskMoney[index])
     task.splice(index, 1)
     taskMoney.splice(index,1)
-    localStorage.setItem("task", JSON.stringify(task))
-    localStorage.setItem("taskMoney", JSON.stringify(taskMoney))
-    render()
+
+    saveInLocalStorage()
+    render(money, task, taskMoney)
 }
 
 
 resetBtnEl.addEventListener("click", function () {
-    localStorage.setItem("money", "0")
-    localStorage.setItem("task", "[]")
-    localStorage.setItem("taskMoney","[]")
-    render()
+    money = 0, task = [], taskMoney = []
+    localStorage.clear()
+    render(money, task, taskMoney)
 })
+
+function saveInLocalStorage() {
+    localStorage.setItem("money", JSON.stringify(money))
+    localStorage.setItem("task", JSON.stringify(task))
+    localStorage.setItem("taskMoney", JSON.stringify(taskMoney))
+}
